4794c1458740173ff5b708e4ec493e656be1014d
saxenanickk <saxenanickk@gmail.com>
2024-01-30 12:35:36 +0530

    Manifest route added to the application (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import manifestRoute from "./manifest";
 
 dotenv.config();
 
@@ -7,8 +8,10 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
+  res.send("<h1>OTA Updates Service is up and running.</h1>");
 });
+
+app.use("/manifest", manifestRoute);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
